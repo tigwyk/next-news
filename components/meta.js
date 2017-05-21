e7821f0cb4d9c4036ce853751eb2ff8fc908d0ed
John Brett <johnbrett7@gmail.com>
2017-05-21 04:40:52 +0100

    add favicon (#20)
    This PR adds a favicon to next-news, as how the next documentation
    suggests adding static content:
    https://github.com/zeit/next.js/#static-file-serving-eg-images.

--- components/meta.js
+++ components/meta.js
@@ -11,6 +11,7 @@ export default () => (
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
+      <link rel="shortcut icon" href="/static/favicon.ico" />
     </Head>
     <style jsx global>{`
       body {
