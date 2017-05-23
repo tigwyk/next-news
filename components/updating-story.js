0c2c99112fa9a98ccffc640e6b7cdfcdcfe5244f
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2017-05-24 01:15:03 +0530

    Use firebase as a dynamic component. (#21)

--- components/updating-story.js
+++ components/updating-story.js
@@ -10,11 +10,29 @@ export default class extends React.Component {
   }
 
   componentDidMount () {
-    this.unsubscribe = observe(this.props.id, (data) => this.setState(data))
+    // Start observing after 30 secs.
+    // That's because we already have the latest data and no need to
+    // start watching immediately.
+    // With this we could reduce ~100 KB(firebase) initial data loading
+    this.timeoutHandler = setTimeout(() => {
+      this.unsubscribePromise = observe(
+        this.props.id,
+        (data) => this.setState(data)
+      )
+      this.timeoutHandler = null
+    }, 1000 * 30)
   }
 
   componentWillUnmount () {
-    this.unsubscribe()
+    // We haven't started watching yet.
+    // So, clear the timeout and return
+    if (this.timeoutHandler) {
+      clearTimeout(this.timeoutHandler)
+      return
+    }
+
+    this.unsubscribePromise
+      .then(unsubscribe => unsubscribe())
   }
 
   render () {
